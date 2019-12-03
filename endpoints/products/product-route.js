943d5560efca080ceaf7ad90a6b054299d94b95d
Farhan-Nazir <nazirf3440@gmail.com>
2019-12-03 22:08:24 +0100

    issue #8 routing setup is ready for use

---
+++ endpoints/products/product-route.js
@@ -0,0 +1,7 @@
+function productRouter(router) {
+  router.route("/api/products").get((req, res) => {
+    return res.send("Products API working.........");
+  });
+}
+
+module.exports = productRouter;
